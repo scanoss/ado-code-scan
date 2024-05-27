d7e4617e392c340f20bd646f3ed21a214d2e18e5
agusgroh <agusgroh@gmail.com>
2024-05-27 13:59:19 -0300

    SP-756 Adds sbom ingestion

---
+++ codescantask/services/result.interface.ts
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: MIT
+/*
+   Copyright (c) 2024, SCANOSS
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+ */
+
+export type ScannerResults = Record<string, ScannerComponent[] | DependencyComponent[]>;
+
+export enum ComponentID {
+    NONE = 'none',
+    FILE = 'file',
+    SNIPPET = 'snippet',
+    DEPENDENCY = 'dependency'
+}
+
+interface CommonComponent {
+    id: ComponentID;
+    status: string;
+}
+
+export interface DependencyComponent extends CommonComponent {
+    dependencies: {
+        licenses: {
+            is_spdx_approved: boolean;
+            name: string;
+            spdx_id: string;
+        }[];
+        purl: string;
+        url: string;
+        version: string;
+    }[];
+}
+
+export interface ScannerComponent extends CommonComponent {
+    lines: string;
+    oss_lines: string;
+    matched: string;
+    purl: string[];
+    vendor: string;
+    component: string;
+    version: string;
+    latest: string;
+    url: string;
+    release_date: string;
+    file: string;
+    url_hash: string;
+    file_hash: string;
+    source_hash: string;
+    file_url: string;
+    licenses: {
+        name: string;
+        patent_hints: string;
+        copyleft: string;
+        checklist_url: string;
+        osadl_updated: string;
+        source: string;
+        incompatible_with?: string;
+        url: string;
+    }[];
+    dependencies: {
+        vendor: string;
+        component: string;
+        version: string;
+        source: string;
+    }[];
+    copyrights: {
+        name: string;
+        source: string;
+    }[];
+    vulnerabilities: {
+        ID: string;
+        CVE: string;
+        severity: string;
+        reported: string;
+        introduced: string;
+        patched: string;
+        summary: string;
+        source: string;
+    }[];
+    quality: {
+        score: string;
+        source: string;
+    }[];
+    cryptography: any[];
+    health: {
+        creation_date: string;
+        issues: number;
+        last_push: string;
+        last_update: string;
+        watchers: number;
+        country: string;
+        stars: number;
+        forks: number;
+    };
+    server: {
+        version: string;
+        kb_version: { monthly: string; daily: string };
+        hostname: string;
+        flags: string;
+        elapsed: string;
+    };
+}
--- codescantask/services/scan.service.ts
+++ codescantask/services/scan.service.ts
@@ -10,6 +10,7 @@ import {
     SBOM_ENABLED,
     SBOM_FILEPATH, SBOM_TYPE
 } from '../app.input';
+import { ScannerResults } from './result.interface';
 export interface Options {
     /**
      * Whether SBOM ingestion is enabled. Optional.
@@ -61,42 +62,68 @@ export class ScanService {
             inputFilepath: REPO_DIR
         };
     }
-    async scan() {
+    async scan(): Promise<ScannerResults> {
         try {
-            try {
-                const dockerCommand = this.buildCommand();
-                console.log(`Executing Docker command: ${dockerCommand}`);
+            const dockerCommand = await this.buildCommand();
+            console.log(`Executing Docker command: ${dockerCommand}`);
 
-                const options = {
-                    failOnStdErr: false,
-                    ignoreReturnCode: true,
-                };
-                const resultCode = await tl.execAsync('bash', ['-c', dockerCommand], options);
+            const options = {
+                failOnStdErr: false,
+                ignoreReturnCode: true,
+            };
+            const resultCode = await tl.execAsync('bash', ['-c', dockerCommand], options);
 
-                if (resultCode !== 0) {
-                    console.error(`Error: Docker command failed with exit code ${resultCode}`);
-                    tl.setResult(tl.TaskResult.Failed, 'Docker command failed');
-                } else {
-                    console.log('Docker command executed successfully');
-                    this.uploadResultsToArtifacts();
-                }
-            } catch (error: any) {
-                console.error('Error:', error);
-                tl.setResult(tl.TaskResult.Failed, error.message);
+            if (resultCode !== 0) {
+                console.error(`Error: Docker command failed with exit code ${resultCode}`);
+                tl.setResult(tl.TaskResult.Failed, 'Docker command failed');
             }
+            console.log('Docker command executed successfully');
+            this.uploadResultsToArtifacts();
+            return this.parseResult();
+
 
         } catch (error: any) {
+            console.error('Error:', error);
             tl.setResult(tl.TaskResult.Failed, error.message);
             throw error;
         }
     }
 
-    private buildCommand(): string {
-       return `docker run -v "${this.options.inputFilepath}":"/scanoss" ghcr.io/scanoss/scanoss-py:v1.9.0 scan . --output ./results.json ${this.options.dependenciesEnabled ? `--dependencies` : ''} ${this.options.apiUrl ? `--apiurl ${this.options.apiUrl}` : ''} ${this.options.apiKey ? `--key ${this.options.apiKey}` : ''}`.replace(/\n/gm, ' ');
+    private async buildCommand(): Promise<string> {
+       return `docker run -v "${this.options.inputFilepath}":"/scanoss" ghcr.io/scanoss/scanoss-py:v1.9.0 scan . --output ./results.json ${this.options.dependenciesEnabled ? `--dependencies` : ''} ${await this.detectSBOM()}  ${this.options.apiUrl ? `--apiurl ${this.options.apiUrl}` : ''} ${this.options.apiKey ? `--key ${this.options.apiKey}` : ''}`.replace(/\n/gm, ' ');
     }
 
     private uploadResultsToArtifacts(){
         const artifactName = 'scanoss';
         tl.command('artifact.upload', { artifactname: artifactName }, this.options.outputFilepath);
+    }
+
+    /**
+     * Constructs the command segment for SBOM ingestion based on the current configuration. This method checks if SBOM
+     * ingestion is enabled and verifies the SBOM file's existence before constructing the command.
+     *
+     * @example
+     * // When SBOM ingestion is enabled with a specified SBOM file and type:
+     * // sbomEnabled = true, sbomFilepath = "/src/SBOM.json", sbomType = "identify"
+     * // returns "--identify /src/SBOM.json"
+     *
+     * @returns A command string segment for SBOM ingestion or an empty string if conditions are not met.
+     * @private
+     */
+    private async detectSBOM(): Promise<string> {
+        if (!this.options.sbomEnabled || !this.options.sbomFilepath) return '';
+
+        try {
+            await fs.promises.access(this.options.sbomFilepath, fs.constants.F_OK);
+            return `--${this.options.sbomType} ${this.options.sbomFilepath}`;
+        } catch (error:any) {
+            tl.setResult(tl.TaskResult.SucceededWithIssues, error.message);
+            return '';
+        }
+    }
+
+    private async parseResult(): Promise<ScannerResults> {
+        const content = await fs.promises.readFile(this.options.outputFilepath, 'utf-8');
+        return JSON.parse(content) as ScannerResults;
     }
 }
