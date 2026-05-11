4d05da0aab260e519d73129f7db246b5f1737352
Agustin Groh <agusgroh@gmail.com>
2026-05-11 10:30:04 -0300

    chore(runtime):SP-4382 bump runtime container to v1.52.1

--- codescantask/services/scan.service.ts
+++ codescantask/services/scan.service.ts
@@ -71,7 +71,7 @@ export interface Options {
     inputFilepath: string;
 
     /**
-     * Runtime container to perform scan. Default [ghcr.io/scanoss/scanoss-py:v1.45.0]
+     * Runtime container to perform scan. Default [ghcr.io/scanoss/scanoss-py:v1.52.1]
      */
     runtimeContainer: string;
 
